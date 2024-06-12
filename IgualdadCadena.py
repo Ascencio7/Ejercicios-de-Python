a6fda6ff67d8948a28548fe89159e30d5ba315ed
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-06-12 15:23:17 -0600

    Igualdad de dos cadenas de texto ingresadas.

---
+++ IgualdadCadena.py
@@ -0,0 +1,15 @@
+""" 
+    Escribir un programa que ingrese dos cadenas de texto y verificar si son 
+    iguales o no.
+"""
+print("\nIgualdad de dos cadenas ingresadas")
+
+cadeneOne = input("\nIngrese la primer cadena de texto: ")
+cadeneTwo = input("Ingrese la segunda cadena de texto: ")
+
+if cadeneOne == cadeneTwo:
+    print("\nAmbas cadenas son iguales.")
+    print("Fin del programa.\n")
+else:
+    print("\nLas dos cadenas no son iguales.")
+    print("Fin del programa.\n")
