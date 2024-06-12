fae476eddd52fae6908ca56492d0b6f05efd5e53
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-06-12 14:58:37 -0600

    La mitad entera de un número ingresado.

---
+++ MitadEnteraNumero.py
@@ -0,0 +1,13 @@
+""" 
+    Escribir un programa que reciba un número y que se obtenga su mitad entera
+"""
+
+print("\nMitad entera de un número ingresado:")
+
+num = float(input("\nIngresa un número: "))
+
+#Half = Mitad
+half = num // 2
+
+print(f"\nLa mitad entera de {num} es: {half}")
+print("Fin del programa.\n")
