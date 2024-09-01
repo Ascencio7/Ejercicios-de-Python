edc71a3580e8c5558971512d7fcb9be10d84322e
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-08-31 20:29:44 -0600

    Polinomio con numeros fraccionarios

---
+++ Polinomio.py
@@ -0,0 +1,14 @@
+from fractions import Fraction
+
+# Representan a numeros fraccionarios
+x = Fraction(10,2)
+y = Fraction(2,1)
+
+# Se imprimen los resultados de las dos variables
+print("\n", x)
+print("\n", y)
+
+# Resta de un polinomio
+z = Fraction(-3,4) - 1 - Fraction(2,3)
+
+print(z) 
